ef2f949397d5cb08cd376f49e89a3f4de6196205
ferblasco7 <ferblasco7@gmail.com>
2020-11-10 12:36:30 +0100

    Update potencias.py

--- potencias.py
+++ potencias.py
@@ -47,3 +47,21 @@ for uno in range(2,10):
         print(r'\begin{multi}[points=1]{Potencias}') #Hay que imprimirla "en crudo"
         print(variedad2.format(uno, dos, tres, tres) + '\n\n')
 
+
+## Variedad 3: Descomponer y extraer
+
+variedad3=''' $\sqrt[3]{{ {producto_todos} }}$ es igual a... 
+\item* ${primero} \sqrt[3]{{ {segundo} }}$ 
+\item ${prod} \sqrt{{ {segundo} }}$ 
+\item ${prod} \sqrt[3]{{ {segundo} }}$ 
+\item Nada de lo anterior
+\end{{multi}}''' 
+
+output=[]
+
+for uno in range(2,6):
+    for dos in [3,5,7]: 
+        #output.append(variedad1.format(uno, dos, tres) + '\n\n')
+        print(r'\begin{multi}[points=1]{Potencias}') #Hay que imprimirla "en crudo"
+        print(variedad3.format(primero=uno,segundo=dos*dos,producto_todos=uno**3*dos**2,prod=uno*dos) + '\n\n')
+
