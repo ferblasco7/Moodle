c88e20ee1ed69e3bf3b8a7147086d49aa7d8f52e
ferblasco7 <ferblasco7@gmail.com>
2020-11-10 10:01:56 +0100

    Update potencias.py

--- potencias.py
+++ potencias.py
@@ -6,19 +6,22 @@ Created on Wed Feb 12 17:49:09 2020
 @author: fer
 """
 
-variedad1='''\begin{{multi}}[points=1]{{Potencias}}
-Escribe ${0}^{{{1}/{2}}}$ en forma de raíz. 
+#No ponemos la primera línea porque el \begin da problemas (\b es un carácter especial en python)
+
+variedad1='''Escribe ${0}^{{{1}/{2}}}$ en forma de raíz. 
 \item* $\sqrt[{2}]{{{0}^{1}}}$
 \item $\sqrt[{1}]{{{0}^{2}}}$
 \item $\sqrt{{{0}^{{{1}/{2}}}}}$
 \item Nada de lo anterior
-\end{{multi}}'''
+\end{{multi}}''' 
 
-output=''
+output=[]
 
-for uno in range(1,10):
+for uno in range(2,10):
     for dos in range(uno+1,10):
         for tres in range(dos+1,10):
-            output = output + variedad1.format(uno, dos, tres) + '\n\n'
+            output.append(variedad1.format(uno, dos, tres) + '\n\n')
+            print(r'\begin{multi}[points=1]{Potencias}') #Hay que imprimirla "en crudo"
+            print(variedad1.format(uno, dos, tres) + '\n\n')
 
-            
+          
