4a831c1bc24c932f2d51d134de19884403ccd0cf
ferblasco7 <ferblasco7@gmail.com>
2020-11-10 11:38:30 +0100

    Update potencias.py

--- potencias.py
+++ potencias.py
@@ -11,6 +11,8 @@ Pensada para usar junto con el paquete moodle de LaTeX, para crear cuestionarios
 
 #No ponemos la primera línea porque el \begin da problemas (\b es un carácter especial en python)
 
+## Variedad 1: Escribir en forma de raíz
+
 variedad1='''Escribe ${0}^{{{1}/{2}}}$ en forma de raíz. 
 \item* $\sqrt[{2}]{{{0}^{1}}}$
 \item $\sqrt[{1}]{{{0}^{2}}}$
@@ -28,4 +30,22 @@ for uno in range(2,10):
             print(r'\begin{multi}[points=1]{Potencias}') #Hay que imprimirla "en crudo"
             print(variedad1.format(uno, dos, tres) + '\n\n')
 
-          
+## Variedad 2: Invertir y simplificar
+
+variedad2=''' ${0}^{{-{1}/{2}}}$ es igual a... 
+\item* $\frac{{1}}{{{0}^{3} }}
+\item $\sqrt[{1}]{{{0}^{2}}}$
+\item $\sqrt{{{0}^{{{1}/{2}}}}}$
+\item Nada de lo anterior
+\end{{multi}}''' 
+
+output=[]
+
+for uno in range(2,10):
+    for dos in range(2,10):
+        for tres in range(2,10):
+            if tres==dos: continue
+            output.append(variedad1.format(uno, dos, tres) + '\n\n')
+            print(r'\begin{multi}[points=1]{Potencias}') #Hay que imprimirla "en crudo"
+            print(variedad1.format(uno, dos, tres, dos/tres) + '\n\n')
+
